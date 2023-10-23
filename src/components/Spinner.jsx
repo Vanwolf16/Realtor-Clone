58ca8039e0f8a7d386b126448e87af8994d29fa6
vanwolf16 <d.murillo28@yahoo.com>
2023-10-23 15:20:12 -0700

    Created a create-list page with forms

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,8 @@ import spinner from "../assets/svg/spinner.svg"
 
 export const Spinner = () => {
     return(
-        <div>
+        <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0
+        top-0 z-50">
             <div>
                 <img src={spinner} alt="loading..." 
                 className="h-24"/>
